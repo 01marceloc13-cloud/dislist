64edfad60d6f77ac1d3c6ce2bb499511b8a8f2f8
Marcelo Campos <01marceloc13@gmail.com>
2025-08-19 16:42:01 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,22 +20,28 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String Platform;
+	private String Platforms;
+	private Double score;
     private String imgUrl;
+    
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
     
     public Game() {
     	
     }
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		Platform = platform;
+		this.Platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -73,12 +79,21 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return Platform;
+	public String getPlatforms() {
+		return Platforms;
 	}
 
 	public void setPlatform(String platform) {
-		Platform = platform;
+		Platforms = platform;
+	}
+	
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
